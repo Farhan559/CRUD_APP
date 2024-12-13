8b877fbb03c9b2cb8011e6cba72503f7fd12c34a
Farhan559 <farhansiddiqui559@gmail.com>
2024-12-13 20:20:19 +0500

    feat: Added Api Calls

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,3 +5,16 @@ export const getAllItems = async()=>{
     const response = await axios.get(API_URL);
     return response.data;
 }
+export const createItem = async(item)=>{
+    const response = await axios.post(API_URL);
+    return response.data;
+
+}
+export const updateItem = async(id , item)=>{
+    const response = await axios.put(`${API_URL}/${id}`,item);
+    return response.data;
+}
+export const deleteItem = async(id)=>{
+    const response = await axios.delete(`${API_URL}/${id}`);
+    return response.data;
+}
